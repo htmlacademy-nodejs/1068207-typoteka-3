99e55143ec36ff41967459e8f751c60df13ce826
aldeimos <50231229+aldeimos@users.noreply.github.com>
2020-11-25 23:26:23 +0300

    Личный проект: другой сервер (#9)
    * Личный проект: по тропе Аппалачей
    
    * Личный проект: шаблонизируй это
    
    * Личный проект: другой сервер

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,67 +1,24 @@
 'use strict';
 
-const chalk = require(`chalk`);
-const http = require(`http`);
-const fs = require(`fs`).promises;
+const express = require(`express`);
+const articlesRouter = require(`../routes/offers`);
 
-const PORT = 3000;
-const MOCK_FILE = `mocks.json`;
-const {HttpCode, ExitCode} = require(`../utils/_constants`);
-
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  res.end(template);
-};
-
-const handleClientConntection = async (req, res) => {
-  switch (req.url) {
-    case `/`:
-      try {
-        const rawJson = await fs.readFile(MOCK_FILE);
-        const publications = JSON.parse(rawJson)
-          .map((publication) => `<li>${publication.title}</li>`)
-          .join(``);
-        return sendResponse(res, HttpCode.OK, publications);
-      } catch (error) {
-        console.log(chalk.red(error));
-        return sendResponse(res, HttpCode.NOT_FOUND, `Not Found`);
-      }
-    default: return sendResponse(res, HttpCode.NOT_FOUND, `Упс, такой страницы нет, 404`);
-  }
-};
-
-const handleServerConnection = (error, port) => {
-  if (error) {
-    console.error(chalk.red(`Ошибка при создании сервера, ${error}`));
-    process.exit(ExitCode.error);
-  }
-
-  return console.info(chalk.green(`Ожидаю соединений на ${port}`));
-};
-
+const DEFAULT_PORT = 3000;
 
 module.exports = {
   name: `--server`,
   async run(args) {
     const [customPort] = args;
 
-    const port = parseInt(customPort, 10) || PORT;
+    const port = Number(parseInt(customPort, 10)) || DEFAULT_PORT;
+    const app = express();
+    app.use(express.json());
+    app.use(`/articles`, articlesRouter);
 
-    http
-      .createServer(handleClientConntection)
-      .listen(port)
-      .on(`listening`, (error) => handleServerConnection(error, port));
+    app.get(`/`, async (req, res) => {
+      return res.send(`Hello, Api Service!`);
+    });
+
+    app.listen(port, () => console.log(`Server has been started at ${port}`));
   }
 };
